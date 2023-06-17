0cd8315e74c23dac2eab9c051fbfd5fd1351bf14
anvimaa <70594573+anvimaa@users.noreply.github.com>
2023-06-17 12:59:18 +0100

    ok

--- BumbaApiGraphQL/Program.cs
+++ BumbaApiGraphQL/Program.cs
@@ -1,12 +1,17 @@
 using BumbaApiGraphQL.Data;
+using BumbaApiGraphQL.Types;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-
 var connectionString = builder.Configuration.GetConnectionString("todos") ?? "Data Source=todos.db";
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(connectionString));
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<QueryType>()
+    .AddMutationType<MutationType>();
 
 var app = builder.Build();
 
--- BumbaApiGraphQL/Types/MutationType.cs
+++ BumbaApiGraphQL/Types/MutationType.cs
@@ -1,5 +1,14 @@
-﻿namespace BumbaApiGraphQL.Types;
+﻿using BumbaApiGraphQL.Data;
+using BumbaApiGraphQL.Models;
+
+namespace BumbaApiGraphQL.Types;
 
 public class MutationType
 {
+    public async Task<User> AddUser([Service] DataContext ctx, User input)
+    {
+        ctx.User.Add(input);
+        await ctx.SaveChangesAsync();
+        return input;
+    }
 }
